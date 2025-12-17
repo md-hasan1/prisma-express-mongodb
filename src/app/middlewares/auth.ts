85dc0dc04cb3808085e049f2bf391c93f9e22dd0
md-hasan1 <akonhasan680@gmail.com>
2025-12-17 16:32:47 +0600

    refactor: improve token extraction logic in auth middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -15,7 +15,8 @@ const auth = (...roles: string[]) => {
     next: NextFunction
   ) => {
     try {
-      const token = req.headers.authorization;
+ const token = req.headers.authorization?.split(" ").includes("Bearer") ? req.headers.authorization?.split(" ")[1] : req.headers.authorization;
+
 
       if (!token) {
         throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized!");
